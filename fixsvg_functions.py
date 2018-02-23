d9f0a1a27aaea62d01235d4240b50820ce5d0d35
ssookhan <shane.sookhan@mail.utoronto.ca>
2018-02-22 23:13:56 -0500

    updated to adjust style and fix tab index

--- fixsvg_functions.py
+++ fixsvg_functions.py
@@ -19,12 +19,11 @@ def indexmaker( foldername,filename ):
     tab = []
     
     for line in table:
-    	sline = line.strip().split(',')
-    	tab.append(sline)
+        sline = line.strip().split(',')
+        tab.append(sline)
     
     
     prog = re.compile('path id=.*?" ')
-    path = re.compile('path')
     
     lines = open(infile, 'r').readlines()
     o = open(outfile_name, 'w')
@@ -48,16 +47,16 @@ def indexmaker( foldername,filename ):
     o.write('<head>\n')
     o.write('  <link rel="stylesheet" type="text/css" href="https://planetearth.utsc.utoronto.ca/diagrams/tooltipster.bundle.min.css"/>\n')
     o.write('  <link rel="stylesheet" type="text/css" href="https://planetearth.utsc.utoronto.ca/diagrams/tooltipster-follower.min.css"/>\n')
-    o.write('  <link rel="stylesheet" type="text/css" href="https://planetearth.utsc.utoronto.ca/diagrams/style.css?v=1.0"/>\n')
+    o.write('  <link rel="stylesheet" type="text/css" href="https://planetearth.utsc.utoronto.ca/diagrams/style.css?v=1.1"/>\n')
     o.write('  <script type="text/javascript" src="https://code.jquery.com/jquery-1.10.0.min.js"></script>\n')
     o.write('  <script type="text/javascript" src="https://planetearth.utsc.utoronto.ca/diagrams/tooltipster.bundle.min.js"></script>\n')
     o.write('  <script type="text/javascript" src="https://planetearth.utsc.utoronto.ca/diagrams/tooltipster-follower.min.js"></script>\n')
     o.write('  <script>\n')
     o.write('  $(document).ready(function() {\n')
     o.write("      $('.tooltip').tooltipster({\n")
-    o.write("	  plugins: ['follower'],\n")
-    o.write("	  contentAsHTML: true,\n")
-    o.write("	  trigger: 'custom', // add custom trigger\n")
+    o.write("      plugins: ['follower'],\n")
+    o.write("      contentAsHTML: true,\n")
+    o.write("      trigger: 'custom', // add custom trigger\n")
     o.write("       triggerOpen: { // open tooltip when element is clicked, tapped (mobile) or hovered\n")
     o.write("           click: true,\n")
     o.write("           tap: true,\n")
@@ -78,44 +77,46 @@ def indexmaker( foldername,filename ):
     o.write("\n")
     o.write("<body>\n")
     o.write('  <a href="full.html" target="_blank">\n')
-    o.write('    <img src="https://planetearth.utsc.utoronto.ca/diagrams/media-fullscreen.png" alt="Expand image">\n')
+    o.write('    <img src="https://planetearth.utsc.utoronto.ca/diagrams/media-fullscreen.png" alt="Expand image" id="expandfull">\n')
     o.write("  </a>\n")
     o.write('<div class="container">\n')
     stforlin= '<svg id="InteractiveFigure" height="100%" width="100%" ' + viewbox + ' version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-label="'+aria_label+'" aria-describedby="map-desc" role="group">\n'
     o.write(stforlin)
-    st2forlin = '	<desc id="map-desc">'+aria_desc+'</desc>\n'
+    st2forlin = '    <desc id="map-desc">'+aria_desc+'</desc>\n'
     o.write(st2forlin)
     
     
     ################################################################################################################
     start_writing=False
     starting_line = re.compile('g id')
+    counts= 1
     
     for line in lines:
-    	m = prog.search(line)
-    	m2 = starting_line.search(line)
-    	if m2:
-    		start_writing = True
-    	if m:
-    		#print( line )
-    		path_id = m.group()
-    		path_id = path_id.strip('path id=').strip('\"').strip()
-    		new_line = ''
-    		for row in tab:
-    			pid = row[1]
-    			#if path_id == 'Cordillera':
-    			#	pdb.set_trace()
-    			if  pid == path_id:
-    				new_string = 'path class="tooltip" title="<strong>%s</strong><br>%s" xlink:href="#%s" role="img" aria-label="%s" tabindex="1" '%(row[2], row[3], row[1], row[5])
-    				new_line = re.sub('path', new_string, line)
-    		if start_writing:
-    			if new_line == '':
-    				o.write(line)
-    			else:
-    				o.write(new_line)
-    	else:
-    		if start_writing: 
-    			o.write(line)
+        m = prog.search(line)
+        m2 = starting_line.search(line)
+        if m2:
+            start_writing = True
+        if m:
+            #print( line )
+            path_id = m.group()
+            path_id = path_id.strip('path id=').strip('\"').strip()
+            new_line = ''
+            for row in tab:
+                pid = row[1]
+                #if path_id == 'Cordillera':
+                #    pdb.set_trace()
+                if  pid == path_id:
+                    new_string = 'path class="tooltip" title="<strong>%s</strong><br>%s" xlink:href="#%s" role="img" aria-label="%s" tabindex="%s" '%(row[2], row[3], row[1], row[5], counts)
+                    counts = counts + 1
+                    new_line = re.sub('path', new_string, line)
+            if start_writing:
+                if new_line == '':
+                    o.write(line)
+                else:
+                    o.write(new_line)
+        else:
+            if start_writing: 
+                o.write(line)
     #################################################################################################################
     o.write('\n')
     o.write('</div>\n')
@@ -139,12 +140,11 @@ def fullmaker( foldername,filename ):
     tab = []
     
     for line in table:
-    	sline = line.strip().split(',')
-    	tab.append(sline)
+        sline = line.strip().split(',')
+        tab.append(sline)
     
     
     prog = re.compile('path id=.*?" ')
-    path = re.compile('path')
     
     lines = open(infile, 'r').readlines()
     o = open(outfile_name, 'w')
@@ -168,16 +168,16 @@ def fullmaker( foldername,filename ):
     o.write('<head>\n')
     o.write('  <link rel="stylesheet" type="text/css" href="https://planetearth.utsc.utoronto.ca/diagrams/tooltipster.bundle.min.css"/>\n')
     o.write('  <link rel="stylesheet" type="text/css" href="https://planetearth.utsc.utoronto.ca/diagrams/tooltipster-follower.min.css"/>\n')
-    o.write('  <link rel="stylesheet" type="text/css" href="https://planetearth.utsc.utoronto.ca/diagrams/style.css?v=1.0"/>\n')
+    o.write('  <link rel="stylesheet" type="text/css" href="https://planetearth.utsc.utoronto.ca/diagrams/style.css?v=1.1"/>\n')
     o.write('  <script type="text/javascript" src="https://code.jquery.com/jquery-1.10.0.min.js"></script>\n')
     o.write('  <script type="text/javascript" src="https://planetearth.utsc.utoronto.ca/diagrams/tooltipster.bundle.min.js"></script>\n')
     o.write('  <script type="text/javascript" src="https://planetearth.utsc.utoronto.ca/diagrams/tooltipster-follower.min.js"></script>\n')
     o.write('  <script>\n')
     o.write('  $(document).ready(function() {\n')
     o.write("      $('.tooltip').tooltipster({\n")
-    o.write("	  plugins: ['follower'],\n")
-    o.write("	  contentAsHTML: true,\n")
-    o.write("	  trigger: 'custom', // add custom trigger\n")
+    o.write("      plugins: ['follower'],\n")
+    o.write("      contentAsHTML: true,\n")
+    o.write("      trigger: 'custom', // add custom trigger\n")
     o.write("       triggerOpen: { // open tooltip when element is clicked, tapped (mobile) or hovered\n")
     o.write("           click: true,\n")
     o.write("           tap: true,\n")
@@ -200,39 +200,40 @@ def fullmaker( foldername,filename ):
     o.write('<div class="container">\n')
     stforlin= '<svg id="InteractiveFigure" height="100%" width="100%" ' + viewbox + ' version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-label="'+aria_label+'" aria-describedby="map-desc" role="group">\n'
     o.write(stforlin)
-    st2forlin = '	<desc id="map-desc">'+aria_desc+'</desc>\n'
+    st2forlin = '    <desc id="map-desc">'+aria_desc+'</desc>\n'
     o.write(st2forlin)
     
     
     ################################################################################################################
     start_writing=False
     starting_line = re.compile('g id')
-    
+    counts = 1
     for line in lines:
-    	m = prog.search(line)
-    	m2 = starting_line.search(line)
-    	if m2:
-    		start_writing = True
-    	if m:
-    		#print( line )
-    		path_id = m.group()
-    		path_id = path_id.strip('path id=').strip('\"').strip()
-    		new_line = ''
-    		for row in tab:
-    			pid = row[1]
-    			#if path_id == 'Cordillera':
-    			#	pdb.set_trace()
-    			if  pid == path_id:
-    				new_string = 'path class="tooltip" title="<strong>%s</strong><br>%s" xlink:href="#%s" role="img" aria-label="%s" tabindex="1" '%(row[2], row[3], row[1], row[5])
-    				new_line = re.sub('path', new_string, line)
-    		if start_writing:
-    			if new_line == '':
-    				o.write(line)
-    			else:
-    				o.write(new_line)
-    	else:
-    		if start_writing: 
-    			o.write(line)
+        m = prog.search(line)
+        m2 = starting_line.search(line)
+        if m2:
+            start_writing = True
+        if m:
+            #print( line )
+            path_id = m.group()
+            path_id = path_id.strip('path id=').strip('\"').strip()
+            new_line = ''
+            for row in tab:
+                pid = row[1]
+                #if path_id == 'Cordillera':
+                #    pdb.set_trace()
+                if  pid == path_id:
+                    new_string = 'path class="tooltip" title="<strong>%s</strong><br>%s" xlink:href="#%s" role="img" aria-label="%s" tabindex="%s" '%(row[2], row[3], row[1], row[5], counts)
+                    counts = counts + 1
+                    new_line = re.sub('path', new_string, line)
+            if start_writing:
+                if new_line == '':
+                    o.write(line)
+                else:
+                    o.write(new_line)
+        else:
+            if start_writing: 
+                o.write(line)
     #################################################################################################################
     o.write('\n')
     o.write('</div>\n')
